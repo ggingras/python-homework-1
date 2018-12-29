f66b7dbec0e1021298cb279bd4ba508dd140db0e
Gaetan Gingras <gaetan.gingras@usherbrooke.ca>
2018-12-29 15:18:18 -0500

    Update main.py
    Update file comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 """
-Pirple homework 1. Understanding variables in python.
+Pirple Python is Easy
+
+Homework 1 - Understanding variables in python.
 
 Information about my favorite song Learning to Fly
 """
